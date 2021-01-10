c43b9e0077d69a3c83aea41cc8168f3e48ff8587
Estevão Naval <navalestevao@gmail.com>
2021-01-10 00:27:50 -0300

    Adicionando orientação a objeto ao jogo

--- src/script.js
+++ src/script.js
@@ -1,176 +1,172 @@
-const dino = document.querySelector(".dino");
 const background = document.querySelector(".background")
-let isJumping = false;
-let position = 0;
-let score = 0;
-let hightScore = 0;
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highScore");
 
+class Dino {
+    #dinoHTML;
+    #isJumping = false;
+    #dinoPosition = 0;
+
+
+    constructor(divDino) {
+        this.#dinoHTML = divDino;
+    };
+
+    getDinoPosition() { 
+        let dinoPosition = this.#dinoPosition;
+        return dinoPosition;
+    };
+
+    getIsJumping() {
+        let isJumping = this.#isJumping;
+        return isJumping;
+    }
+
+    jump(){
+        this.#isJumping = true;
+        let dinoPosition = this.#dinoPosition;
+        let dinoHTML = this.#dinoHTML;
+
+        let upInterval = setInterval(() => {
+            if(dinoPosition >= 150){
+                clearInterval(upInterval);
+
+                let downInterval = setInterval(() => {
+                    if(dinoPosition === 0){
+                        clearInterval(downInterval)
+                        this.#isJumping = false;
+                    }else{
+                        dinoPosition -= 20;
+
+                        dinoHTML.style.bottom = `${dinoPosition}px`;
+                    }
+                }, 25 + 9.8)
+            } else{
+                dinoPosition += 20;
+
+                dinoHTML.style.bottom = `${dinoPosition}px`;
+            }
+        }, 25 + 9.8);
+
+    };
+
+    lowerDino() {
+        let dinoHTML = this.#dinoHTML;
+        dinoHTML.setAttribute("class","dinoLowered");
+    };
+
+    upDino() {
+        let dinoHTML = this.#dinoHTML;
+        dinoHTML.setAttribute("class", "dino");
+    };
+};
+
+class Score {
+    #score = 0;
+
+    getHighScore() {
+        let highScore = localStorage.getItem('highScore');
+        return highScore;
+    };
+
+    setHighScore() {
+        let score = this.#score;
+        let highScore = localStorage.getItem('highScore') 
+
+        if(score > parseInt(highScore)){
+            localStorage.setItem('highScore', score);
+        }
+    };
+
+    getScore() {
+        let score = this.#score;
+        return score 
+    };
+
+    setScore() { 
+        let score = 0;
+        score += 1;
+
+        this.#score += score;
+    };
+}
+
+class Obstacle {
+    #obstacle = document.createElement("div");
+    #nameClassObstacle;
+    #obstaclePosition = 1000;
+    #randomTime = Math.random() * 6000;
+
+    constructor(nameObstacle, objScore, objDino, intervalScore) {
+        this.#nameClassObstacle = nameObstacle;
+        this.addClassObstacle();
+        this.animateObstacle(objScore, objDino, intervalScore);
+
+        setTimeout(() => {let objObstacle = new Obstacle(nameObstacle, objScore, objDino, intervalScore)}, this.#randomTime)
+    }
+
+    addClassObstacle() {
+        let nameClassObstacle = this.#nameClassObstacle;
+        let obstacle = this.#obstacle;
+
+        obstacle.classList.add(nameClassObstacle);
+        background.appendChild(obstacle);
+        obstacle.style.left = `1000px`;
+    }
+
+    animateObstacle(objScore, objDino, intervalScore) {
+        let obstaclePosition = this.#obstaclePosition;
+        let obstacle = this.#obstacle;
+
+        let intervalLeftObstacle = setInterval(() => {
+            if(obstaclePosition < -60) {
+                clearInterval(intervalLeftObstacle);
+                background.removeChild(obstacle);
+            } /* else if (obstaclePosition > 0 && obstaclePosition < 120 && objDino.getDinoPosition() < 60) {
+                // Game over
+                objScore.setHighScore();
+    
+                clearInterval(intervalLeftObstacle);
+                clearInterval(intervalScore);
+    
+                document.body.innerHTML = '<h1 class="game-over">Fim de jogo<br><a href="index.html" style="text-decoration:none;" class="game-over">Tente novamente!<br><img src="assets/reload.png"></a><h1>';
+            } */else {
+                obstaclePosition -= 10;
+                obstacle.style.left = `${obstaclePosition}px`;
+            }
+
+        }, 20)
+    }
+}
 
 let handleKeyDown = (e) => {
 
-    if (e.keyCode === 40) {
-        lowerDino();
-    } else if (e.keyCode === 32 || e.keyCode === 38){
-        if(!isJumping){
-            jump();
+    if(!objDino.getIsJumping()) {
+        if(e.keyCode === 40) {
+            objDino.lowerDino();
+        } else if(e.keyCode === 32 || e.keyCode === 38) {
+            objDino.jump();
         }
-    }
-    
+    }    
 }
 
 let handleKeyUp = (e) => {
     if (e.keyCode === 40) {
-        upDino();
+        objDino.upDino();
     }
 }
 
-let jump = () => {
-    isJumping = true;
+objDino = new Dino(document.querySelector(".dino"));
+objScore = new Score();
 
-    audio = returnSound("assets/audio/Jump.ogg");
-    audio.play();
+highScore.innerHTML = `Maior pontuação: ${objScore.getHighScore()}`;
 
-    let upInterval = setInterval(() => {
-        if(position >= 150){
-            clearInterval(upInterval);
+let scoreInterval = setInterval(() => {
+    objScore.setScore();
+    score.innerHTML = `Pontuação: ${objScore.getScore()}`;
+}, 34.8)
 
-            let downInterval = setInterval(() => {
-                if(position === 0){
-                    clearInterval(downInterval)
-                    isJumping = false;
-                }else{
-                    position -= 20;
-
-                    dino.style.bottom = `${position}px`;
-                }
-            }, 25 + 9.8)
-        } else{
-            position += 20;
-
-            dino.style.bottom = `${position}px`;
-        }
-    }, 25 + 9.8);
-}
-
-let createCactus = () => {
-    const cactus = document.createElement("div");
-    let cactusPosition = 1000;
-    let randomTime = Math.random() * 6000;
-
-    cactus.classList.add('cactus');
-    cactus.style.left = `${1000}px`;
-    background.appendChild(cactus);
-
-    let leftCactusInterval = setInterval(() => {
-        if(cactusPosition < -60) {
-            clearInterval(leftCactusInterval);
-            background.removeChild(cactus);
-        } else if (cactusPosition > 0 && cactusPosition < 120 && position < 60) {
-            // Game over
-            setHighScore();
-
-            clearInterval(leftCactusInterval);
-            
-            audio = returnSound("assets/audio/fatalDamageCactusSound.ogg");
-            audio.play();
-
-            clearInterval(incrementScoreInterval);
-
-            clearInterval(musicaAmbienteInterval);
-            audio = returnSound("assets/audio/MusicaAmbiente.ogg");
-            audio.pause();
-
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo<br><a href="index.html" style="text-decoration:none;" class="game-over">Tente novamente!<br><img src="assets/reload.png"></a><h1>';
-        } else {
-            cactusPosition -= 10;
-            cactus.style.left = `${cactusPosition}px`;
-        }
-    }, 20);
-
-    setTimeout(createCactus, randomTime);
-};
-
-/* let createBird = () => {
-    const bird = document.createElement("div");
-    let birdPosition = 1000;
-    let randomTime = Math.random() * 6000;
-    let randomPlaceBird = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-
-    bird.classList.add('bird');
-    bird.style.left = `${1000}px`;
-    background.appendChild(bird);
-
-    let leftBirdInterval = setInterval(() => {
-        if(birdPosition < -60){
-            clearInterval(leftBirdInterval);
-            background.removeChild(bird);
-        } else if (birdPosition > 0 && birdPosition < 120 && position < 60) {
-            // Game over
-
-            clearInterval(leftBirdInterval);
-
-            audio = returnSound("assets/audio/fatalDamageBirdSound.ogg");
-            audio.play();
-
-            clearInterval(incrementScoreInterval);
-
-            clearInterval(musicaAmbienteInterval);
-            audio = returnSound("assets/audio/MusicaAmbiente.ogg");
-            audio.pause();
-
-            document.body.innerHTML = '<h1 class="game-over">Fim de jogo<br><a href="index.html" style="text-decoration:none;" class="game-over">Tente novamente!<br><img src="assets/reload.png"></a><h1>';            
-        } else {
-            birdPosition -= 10;
-            bird.style.left = `${birdPosition}px`;
-        }
-    }, 20);
-
-    setTimeout(createBird, randomTime);
-}; */
-
-let lowerDino = () => {
-    dino.setAttribute("class","dinoLowered");
-};
-
-let upDino = () => {
-    dino.setAttribute("class", "dino");
-};
-
-let incrementScore = () => {
-    let htmlScore = document.querySelector(".score");
-    score += 1;
-    htmlScore.innerHTML = `Pontuação: ${score}`;
-};
-
-let setHighScore = () => {
-    if(score > hightScore){
-        localStorage.setItem('highScore', score);
-    }
-}
-
-let getHighScore = () => {
-    hightScore = localStorage.getItem('highScore');
-    return hightScore;
-}
-
-let returnSound = (pathMusic) => {
-    let audio = new Audio(pathMusic);
-    return audio;
-}
-
-let stopSound = (audio) => {
-    audio.pause();
-}
-
-let musicaAmbienteInterval = setInterval(() => {
-    audio = returnSound("assets/audio/MusicaAmbiente.ogg");
-    audio.play();
-}, 326000);
-let incrementScoreInterval = setInterval(incrementScore, 25 + 9.8)
-
-document.querySelector(".highScore").innerHTML += ` ${getHighScore()}`;
-
-createCactus();
+objCactus = new Obstacle("cactus", objScore, objDino, scoreInterval);
 
 document.addEventListener('keydown', handleKeyDown);
 document.addEventListener('keyup', handleKeyUp);
